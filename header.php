a5376eaa42f16fd4130ccf240abf5bb9b2a0f9c7
Paul <paultmorris22@gmail.com>
2025-08-20 15:55:48 -0500

    Basic responsiveness, heading font

--- header.php
+++ header.php
@@ -19,6 +19,8 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Baskervville:ital,wght@0,400..700;1,400..700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 
 	<?php wp_head(); ?>
 </head>
