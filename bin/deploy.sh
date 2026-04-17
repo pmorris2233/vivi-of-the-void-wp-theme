c97a73f9ebd3867b727c3c1b5f6c8665e8a1c00f
Paul Morris <paultmorris22@gmail.com>
2026-04-17 17:22:03 -0500

    Add file/dir checks to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,13 +4,31 @@ working_dir="/home/dh_qi7i23/viviofthevoid.com/wp-content/themes"
 theme="vivi-of-the-void"
 host="dreamhost"
 
+npm run bundle
+
+if [ ! -f "../$theme.zip" ]; then
+	echo '[-] Missing theme zip file'
+	exit 1
+fi
+
 rsync -v "../$theme.zip" "$host:/$working_dir/"
 
 ssh $host /bin/bash << EOF
 	cd "${working_dir}"
+	
+	if [ -d "${theme}.bak" ]; then
+		rm -rf "${theme}.bak"
+	fi
+
 	mv $theme "${theme}.bak"
+
 	unzip "${theme}.zip"
+
 	find $theme -type d -exec chmod 755 {} +
 	find $theme -type f -exec chmod 644 {} +
+
 	rm "${theme}.zip"
 EOF
+
+echo '[+] Deploy complete'
+exit 0
