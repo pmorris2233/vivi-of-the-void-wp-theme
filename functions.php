3c4083639c219bf3b34242784140176da45c522f
Paul Morris <paultmorris22@gmail.com>
2026-04-17 13:26:25 -0500

    Fix theme version constant in functions.php

--- functions.php
+++ functions.php
@@ -138,10 +138,10 @@ add_action( 'widgets_init', 'vivi_of_the_void_widgets_init' );
  * Enqueue scripts and styles.
  */
 function vivi_of_the_void_scripts() {
-	wp_enqueue_style( 'vivi-of-the-void-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'vivi-of-the-void-style', get_stylesheet_uri(), array(), VOTV_VERSION );
 	wp_style_add_data( 'vivi-of-the-void-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'vivi-of-the-void-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'vivi-of-the-void-navigation', get_template_directory_uri() . '/js/navigation.js', array(), VOTV_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
