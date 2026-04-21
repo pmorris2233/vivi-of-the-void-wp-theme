d03c0f2a0b6c6bcb7b854ac457716978e1ee809c
Paul Morris <paultmorris22@gmail.com>
2026-04-21 16:14:10 -0500

    Use .env config file for bash scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,33 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-working_dir="/home/dh_qi7i23/viviofthevoid.com/wp-content/themes"
-theme="vivi-of-the-void"
-host="dreamhost"
+if [ ! -f ".env" ]; then
+	echo "[-] Missing config: '.env' not found"
+	exit 1
+fi
+
+source .env
 
 npm run bundle
 
-if [ ! -f "../$theme.zip" ]; then
+if [ ! -f "../${votv_config[theme]}.zip" ]; then
 	echo '[-] Missing theme zip file'
 	exit 1
 fi
 
-rsync -v "../$theme.zip" "$host:/$working_dir/"
+remote_uri="${votv_config[host]}:/${votv_config[remote_dir]}/wp-content/themes"
 
-ssh $host /bin/bash << EOF
-	cd "${working_dir}"
+rsync -v "../${votv_config[theme]}.zip" "$remote_uri"
+
+ssh ${votv_config[host]} /bin/bash << EOF
+	cd "${votv_config[remote_dir]}/wp-content/themes"
 	
-	if [ -d "${theme}.bak" ]; then
-		rm -rf "${theme}.bak"
+	if [ -d "${votv_config[theme]}.bak" ]; then
+		rm -rf "${votv_config[theme]}.bak"
 	fi
 
-	mv $theme "${theme}.bak"
-
-	unzip "${theme}.zip"
-
-	find $theme -type d -exec chmod 755 {} +
-	find $theme -type f -exec chmod 644 {} +
-
-	rm "${theme}.zip"
+	mv ${votv_config[theme]} "${votv_config[theme]}.bak"
+	unzip "${votv_config[theme]}.zip"
+	find ${votv_config[theme]} -type d -exec chmod 755 {} +
+	find ${votv_config[theme]} -type f -exec chmod 644 {} +
+	rm "${votv_config[theme]}.zip"
 EOF
 
 echo '[+] Deploy complete'
--- bin/pulldb.sh
+++ bin/pulldb.sh
@@ -1,32 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-working_dir="/home/dh_qi7i23/viviofthevoid.com"
-wp_dir="/home/paul/www/viviofthevoid"
-host="dreamhost"
-sql_file="votv.sql"
-download_path="/home/paul/Downloads"
-prod_url="https://www.viviofthevoid.com"
-dev_url="http://votv.localhost:8080"
-
-ssh $host /bin/bash << EOF
-	cd $working_dir	
-
-	if [ -f $sql_file ]; then
-		rm $sql_file
-	fi
-
-	wp db export votv.sql --add-drop-table
-EOF
-
-if [ -f "${download_path}/${sql_file}" ]; then
-	rm "${download_path}/${sql_file}"
+if [ ! -f ".env" ]; then
+	echo "[-] Missing config: '.env' not found"
+	exit 1
 fi
 
-scp "${host}:${working_dir}/${sql_file}" $download_path
+source .env
 
-cd $wp_dir
+echo '[!] Pulling database'
 
-wp db import "${download_path}/${sql_file}"
-wp search-replace "${prod_url}" "${dev_url}"
+ssh ${votv_config[host]} /bin/bash << EOF
+	cd ${votv_config[remote_dir]}	
+
+	if [ -f ${votv_config[sql_file]} ]; then
+		rm ${votv_config[sql_file]}
+	fi
+
+	wp db export ${votv_config[sql_file]} --add-drop-table
+EOF
+
+if [ -f "${votv_config[download_path]}/${votv_config[sql_file]}" ]; then
+	rm "${votv_config[download_path]}/${votv_config[sql_file]}"
+fi
+
+remote_file="${votv_config[host]}:${votv_config[remote_dir]}/${votv_config[sql_file]}"
+
+scp $remote_file ${votv_config[download_path]}
+
+cd ${votv_config[local_wp_dir]}
+
+wp db import "${votv_config[download_path]}/${votv_config[sql_file]}"
+wp search-replace "${votv_config[prod_url]}" "${votv_config[dev_url]}"
+
+echo '[+] DB pull complete'
 
 exit 0
